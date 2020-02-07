c563a30f1f1ca2bacd7f07cedbd060fe189f0cef
haoyuan336 <328387458@qq.com>
2020-02-07 08:29:51 +0800

    多为玩家登录

---
+++ Lesson2_6_3/NiuNiuServer/GameConfig.js
@@ -0,0 +1,31 @@
+module.exports = {
+    RoomConfig: {
+        RoundCount: {
+             "round-count-10": 10,
+             "round-count-20": 20
+        },
+        kouCount: {
+            "kou-type-0": 5,
+            "kou-type-1": 1,
+            "kou-type-2": 2
+        },
+        rateConfig: {
+            "rate-type-0": {
+                "niuniu": 3,
+                "niujiu": 2,
+                "niuba": 2,
+                "niuqi": 2
+            },
+            "rate-type-1": {
+                "niuniu": 4,
+                "niujiu": 3,
+                "niuba": 2,
+                "niuqi": 2
+            }
+        }
+
+    },
+    GameConfig: {
+
+    }
+}
